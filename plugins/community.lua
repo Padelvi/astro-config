cb2c5d8c44b3068c6351af1d779b5c1a0eda1568
Padelvi <pablodelvillarochoa@gmail.com>
2023-04-22 13:30:58 +0200

    Commit at 4-22

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,29 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- Colorscheme
+  { import = "astrocommunity.colorscheme.kanagawa" },
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- Indentation
+  { import = "astrocommunity.indent.indent-tools-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+
+  -- Language packs
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+
+  -- Other
+  { import = "astrocommunity.utility.neodim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.mini-splitjoin" },
+  { import = "astrocommunity.editing-support.nvim-regexplainer" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
+  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
 }
